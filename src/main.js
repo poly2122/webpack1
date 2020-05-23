09559626110294db860811f2853bca279d376e0c
poly <1037533792@qq.com>
2020-05-23 22:08:21 +0800

    vue

---
+++ src/main.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+
+/* import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+*/
+
+import Demo from './components/Demo.vue'
+new Vue({
+  el: '#app',
+
+  render(h) {
+   return (h)(Demo)
+  },
+  
+});
